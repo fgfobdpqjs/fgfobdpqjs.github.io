bf2dd1be1d4e3872992d3461150fc36fdd00d5c1
fgfobdpqjs <142361890+fgfobdpqjs@users.noreply.github.com>
2024-02-16 14:11:47 +0800

    Add files via upload

--- CHICXKgai1/static/index.js
+++ CHICXKgai1/static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://eafoo.github.io/eatcat/static/image/ClickBefore.png';
+var url = 'https://fgfobdpqjs.github.io/CHICXKgai1/static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
